0f45dc44dc5ba9ebf0a56658c862f5c3d8cbbeb9
Joseph Clay <joe@weareimmutable.com>
2021-12-01 12:53:19 -0600

    create deferred

---
+++ src/utils/deferred.js
@@ -0,0 +1,12 @@
+export default function deferred() {
+  let resolve, reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return {
+    resolve,
+    reject,
+    promise,
+  };
+};
